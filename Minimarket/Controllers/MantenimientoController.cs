7be8077053ceccc36e6e7df79e1da10886ad0bdd
Italo Espinoza <Italoespinoza1529@gmail.com>
2025-04-19 17:59:09 -0500

    cambios para dashboard de usuario

--- Minimarket/Controllers/MantenimientoController.cs
+++ Minimarket/Controllers/MantenimientoController.cs
@@ -18,6 +18,52 @@ namespace Minimarket.Controllers
         {
             return View();
         }
+        public async Task<IActionResult> DashboardUsuarios()
+        {
+            var totalUsuarios = await _context.Users.CountAsync();
+
+            var distribucion = await _context.Users
+                .GroupBy(u => u.IdRol)
+                .Select(g => new { RolId = g.Key, Count = g.Count() })
+                .ToListAsync();
+
+            var rolesMap = await _context.Roles
+                .ToDictionaryAsync(r => r.IdRol, r => r.Nombre);
+
+            var rolDistribucion = distribucion
+                .Select(d => new {
+                    Label = rolesMap.ContainsKey(d.RolId) ? rolesMap[d.RolId] : "Desconocido",
+                    Count = d.Count
+                })
+                .ToList();
+
+            var recentUsers = await _context.Users
+                .OrderByDescending(u => u.CreatedAt)
+                .ToListAsync();
+
+            ViewBag.TotalUsuarios = totalUsuarios;
+            ViewBag.RolDistribucion = rolDistribucion;
+            ViewBag.RecentUsers = recentUsers;
+            ViewBag.RolesLabels = rolDistribucion.Select(x => x.Label).ToList();
+            ViewBag.RolesCounts = rolDistribucion.Select(x => x.Count).ToList();
+
+            return PartialView("_DashboardUsuarios");
+        }
+
+        public async Task<IActionResult> DashboardProductos()
+        {
+            var total = await _context.Productos.CountAsync();
+            ViewBag.TotalProductos = total;
+            return PartialView("_DashboardProductos");
+        }
+
+        public async Task<IActionResult> DashboardOrdenes()
+        {
+            var total = await _context.Ordenes.CountAsync();
+            ViewBag.TotalOrdenes = total;
+            return PartialView("_DashboardOrdenes");
+        }
+
 
         public async Task<IActionResult> Productos()
         {
